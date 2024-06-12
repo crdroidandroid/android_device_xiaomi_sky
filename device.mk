2f79bf7ef732043b25c8ecfb1f5ec76cf4208c33
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sky: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -322,7 +322,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay-lineage
+  $(DEVICE_PATH)/overlay \
+  $(DEVICE_PATH)/overlay-lineage
 
 # Partitions
 PRODUCT_PACKAGES += \

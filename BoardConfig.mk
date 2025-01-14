07d60637f96ebd04e5795433746aea7de89a1e9e
Allan DSouza <allan@allan.co.in>
2025-01-14 08:25:46 +0300

    sky: uprev vendor SPL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,7 +224,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_SCREEN_DENSITY := 440
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-10-01
+VENDOR_SECURITY_PATCH := 2024-11-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk

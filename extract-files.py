fcd286532896a5c89d60750054a59d773613c9d9
luk1337 <priv.luk@gmail.com>
2025-03-17 15:06:10 +0000

    sky: Disable T4T_NFCEE >> Fixes NFC on 15 QPR2. >> >> Change-Id: Ib3091187c2f291562f16fcbc05cfa8823b78fe11

--- extract-files.py
+++ extract-files.py
@@ -64,6 +64,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/libnfc-nxp-pnscr.conf': blob_fixup()
+         .regex_replace(
+ 	    'NXP_T4T_NFCEE_ENABLE=0x01', 'NXP_T4T_NFCEE_ENABLE=0x00'
+ 	),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()

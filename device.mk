38284d1a843593056cc354cfa110fb46e4778e85
moetayuko <loli@yuko.moe>
2025-03-17 22:39:36 -0700

    sky: Move to aidl bootctrl hal Change-Id: I50fd09148097f0cda03c1a105340a3fd5f55ce8d

--- device.mk
+++ device.mk
@@ -111,11 +111,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti:64 \
-    android.hardware.boot@1.2-impl-qti.recovery:64 \
-    android.hardware.boot@1.2-service \
-    bootctrl.parrot \
-    bootctrl.parrot.recovery
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery 
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl

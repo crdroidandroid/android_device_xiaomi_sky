fdc3b042e63138a60de927f7a80e6456f8c3292e
Dhanush /.\./. <106550362+dhanush281@users.noreply.github.com>
2025-06-14 08:31:27 -0700

    sky:Update blobs to OS2.0.8.0.VMWEUXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,7 +225,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_SCREEN_DENSITY := 440
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-02-01
+VENDOR_SECURITY_PATCH := 2025-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk

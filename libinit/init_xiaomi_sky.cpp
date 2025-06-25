7a7bdbad751504c95b18208ac2d59f47739668b8
askthekaif <209830895+askthekaif@users.noreply.github.com>
2025-06-25 10:43:47 +0000

    sky: Update blobs to OS2.0.9.0.VMWEUXM

--- libinit/init_xiaomi_sky.cpp
+++ libinit/init_xiaomi_sky.cpp
@@ -9,8 +9,8 @@
 
 #include "vendor_init.h"
 
-#define FINGERPRINT_POCO_IN "POCO/sky_p_in/sky:14/UKQ1.231003.002/OS2.0.3.0.VMWINXM:user/release-keys"
-#define FINGERPRINT_REDMI_IN "Redmi/sky_in/sky:14/UKQ1.231003.002/OS2.0.3.0.VMWINXM:user/release-keys"
+#define FINGERPRINT_POCO_IN "POCO/sky_p_in/sky:12/SKQ1.230118.001/OS2.0.9.0.VMWINXM:user/release-keys"
+#define FINGERPRINT_REDMI_IN "Redmi/sky_in/sky:12/SKQ1.230118.001/OS2.0.9.0.VMWINXM:user/release-keys"
 #define FINGERPRINT_CHINA "Redmi/sky/sky:14/UKQ1.231003.002/V816.0.9.0.UMWCNXM:user/release-keys"
 #define FINGERPRINT_GLOBAL_NFC "Redmi/river_global/river:14/UKQ1.231003.002/V816.0.11.0.UMWMIXM:user/release-keys"
 #define FINGERPRINT_JPN "Redmi/XIG03_jp_kdi/XIG03:14/UKQ1.230917.001/V816.0.5.0.UMWJPKD:user/release-keys"
